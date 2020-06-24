30c5cb7c900b18b1b78206d08ca11a3e0ab8958e
orazweb <jer.paoli@gmail.com>
2020-06-24 12:25:48 +0200

    exercise done

--- TestDate.java
+++ TestDate.java
@@ -18,6 +18,14 @@ class TestDate {
         sc.close();
 
         // TODO : your code after this line
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MM-dd-yyyy");
+        //Attention : le mois récupéré commence à 0 pour Janvier ! Le jour, lui, commence bien à 1.
+        Calendar calendar = new GregorianCalendar(year, month - 1, day);
+        Date date = calendar.getTime();
+        System.out.println("The date is : " + simpleDateFormat.format(date));
+
+        System.out.println("The day of week is : " + calendar.get(Calendar.DAY_OF_WEEK));
         
     }
 }
